61943f4ecae74e247a89ca12bec88a50540a57c5
Osinachukwu Nnanna <contact2amondz24@gmail.com>
2025-07-03 19:16:32 +0100

    added devlog 4,  created searchSong function in index.js

--- bin/index.js
+++ bin/index.js
@@ -1,7 +1,20 @@
 #!/usr/bin/env node //To those who don't know this is a hashbang or as google said aka shebang Tells my computer how to run the file(which interpreter to use)
 import axios from "axios";
-import dotenv from 'dotenv';
-
+import dotenv from "dotenv";
 dotenv.config();
+
 const token = process.env.GENIUS_CLIENT_ACCESS_TOKEN;
-console.log("Genius account token", token)
+const query = process.argv.slice(2).join(" ");
+
+async function searchSong(query) {
+  const res = await axios.get(`https://api.genius.com/search?q=${encodeURIComponent(query)}`, {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  });
+
+  const hits = res.data.response.hits;
+  console.log(hits.map(hit => hit.result.full_title));
+}
+
+searchSong(query);
