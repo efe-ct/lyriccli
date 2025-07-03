c1d00c4ecfc8812c5cb653597d6db36e463528af
Osinachukwu Nnanna <contact2amondz24@gmail.com>
2025-07-03 16:08:54 +0100

    added dotenv, used index.js to test dotenv, grouped devlogs into folders

--- bin/index.js
+++ bin/index.js
@@ -1,1 +1,7 @@
 #!/usr/bin/env node //To those who don't know this is a hashbang or as google said aka shebang Tells my computer how to run the file(which interpreter to use)
+import axios from "axios";
+import dotenv from 'dotenv';
+
+dotenv.config();
+const token = process.env.GENIUS_CLIENT_ACCESS_TOKEN;
+console.log("Genius account token", token)
