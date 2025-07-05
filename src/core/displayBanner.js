929aee899330dd31cc8ea7b04b7957665b04e8cc
Osinachukwu Nnanna <contact2amondz24@gmail.com>
2025-07-05 13:37:37 +0100

    minor: changed font to 3D-ASCII

--- src/core/displayBanner.js
+++ src/core/displayBanner.js
@@ -4,9 +4,9 @@ import gradient from "gradient-string";
 //finally changed directory structure (temporary comment)
 export function displayBanner() {
     const banner = figlet.textSync('LyricCLI',{
-        font: 'Ghost',
-        horizontalLayout: 'default',
-        verticalLayout: 'default'
+        font: '3D-ASCII',
+        horizontalLayout: 'fitted',
+        verticalLayout: 'fitted'
     });
     console.log(gradient.rainbow(banner));
     
